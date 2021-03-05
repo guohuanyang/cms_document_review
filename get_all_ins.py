e4b5d4d16b11fb76e1d2ee61585068efd357add3
guohuanyang <244651200@qq.com>
2021-03-05 10:37:53 +0800

    添加递归终止条件

--- get_all_ins.py
+++ get_all_ins.py
@@ -8,39 +8,67 @@
 # Email         guohuanyang@datagrand.com
 # -------------------------------------------------------------------------------
 from test_api import request_by_ins_num
+from queue import Queue
+task_queue = Queue()
 
 
 class Institution:
     """
     机构信息
     """
-    def __init__(self, name, code, ratio):
+    def __init__(self, name, code, ratio, level=0, parent_ins_num=None, status=False, msg=''):
         self.ins_name = name
         self.ins_num = code
         self.ratio = ratio
-        self.shareholder = list()
+        self.level = level
+        self.parent_ins_num = parent_ins_num
+        self.status = status
+        self.msg = msg
 
-    def get_shareholder(self):
-        if not self.ins_num:
-            return
-        res = request_by_ins_num(self.ins_num)
+
+def bfs_ins():
+    founded_shareholders = list()
+    founded_shareholder_ins = list()
+    while not task_queue.empty():
+        institution = task_queue.get()
+        if not institution.ins_num:
+            institution.status = True
+            institution.msg = 'ins_num不能为空'
+            continue
+        if institution.ins_num in founded_shareholder_ins:
+            institution.msg = '已在前面的层级发现,加入到搜索结果,但不再搜索股东'
+            institution.status = True
+            founded_shareholders.append(institution)
+            continue
+
+        founded_shareholders.append(institution)
+        founded_shareholder_ins.append(institution.ins_num)
+
+        res = request_by_ins_num(institution.ins_num)
         res_data = res.json().get("data", {})
-        total_shareholder = res_data.get("total", 0)
-        if total_shareholder == 0:
-            return
+        if res_data.get("total", 0) == 0:
+            institution.msg = '搜索不到股东信息'
+            institution.status = True
+            continue
+
         data_list = res_data.get("dataList", [])
         for item in data_list:
             shah_name = item.get("shah_name", "")
             shah_num = item.get("shah_num", "")
             hold_ratio = item.get("hold_wght_rati", 0.0)
-            sub_shareholder = Institution(shah_name, shah_num, hold_ratio)
-            sub_shareholder.get_shareholder()
-            print(sub_shareholder)
-            self.shareholder.append(sub_shareholder)
+
+            sub_shareholder = Institution(
+                shah_name, shah_num, hold_ratio,
+                institution.level+1, institution.ins_num)
+            task_queue.put(sub_shareholder)
+
+        institution.status = True
+    return founded_shareholders
 
 
 if __name__ == '__main__':
     test_code = 'cea37295-334a-43a5-9db8-89731f99b096'
-    test_obj = Institution('test', test_code, 100)
-    test_obj.get_shareholder()
-    print(test_obj)
+    test_obj = Institution('test', test_code, 999)
+    task_queue.put(test_obj)
+    shareholder_list = bfs_ins()
+    print(shareholder_list)
